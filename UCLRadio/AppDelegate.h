faa52ce71126982e8c941ec163cd8742f3efa370
Frederic Jacobs <freeric.jacobs@mac.com>
2011-11-24 15:14:19 +0100

    Initial Commit

---
+++ UCLRadio/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UCLRadio
+//
+//  Created by Frederic Jacobs on 24/11/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
